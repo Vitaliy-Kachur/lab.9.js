3e53661ec7c0c31ae8e06bc21dfd1f4a0d391192
Vitaliy <kachur@class.ztk.org.ua>
2023-03-13 18:36:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function EnteringNumbers(numberOfNumbers, sum, enterNumbers) {
+    const numeric = prompt("Введіть число: (для завершення введіть 0)");
+    if (numeric === "0") {
+        let average = sum / numberOfNumbers;
+        console.log(`Введені числа ${enterNumbers} кількість чисел які були введені: ${numberOfNumbers} їх загальна сума: ${sum} та середнє арифметичне: ${average}`);
+    } else {
+        let numeric2 = Number(numeric);
+        sum += numeric2;
+        numberOfNumbers ++;
+        enterNumbers += " "+ numeric + ",";
+        return EnteringNumbers(numberOfNumbers, sum, enterNumbers);
+    }
+  }
+  EnteringNumbers(0, 0, "");
